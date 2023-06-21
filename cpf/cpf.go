112c736e6de5f3001fbbf303bce5116b0b3cfcd0
Bruno Lino <brunolino2026@gmail.com>
2023-06-20 21:27:36 -0300

    test: validating size

--- cpf/cpf.go
+++ cpf/cpf.go
@@ -1,14 +1,27 @@
 package cpf
 
-import "errors"
+import (
+	"errors"
+	"regexp"
+)
 
 type CPF struct {
 	Value string
 }
 
+const (
+	CPF_SIZE = 11
+)
+
 func NewCPF(value string) (cpf *CPF, err error) {
 	if isNull(value) {
 		return cpf, errors.New("invalid CPF")
+	}
+
+	valueClean := clear(value)
+
+	if !hasCorrectSize(valueClean) {
+		return cpf, errors.New("invalid CPF size")
 	}
 
 	return &CPF{Value: value}, nil
@@ -16,4 +29,14 @@ func NewCPF(value string) (cpf *CPF, err error) {
 
 func isNull(value string) bool {
 	return value == ""
+}
+
+func hasCorrectSize(value string) bool {
+	return len(value) == CPF_SIZE
+}
+
+func clear(value string) string {
+	sampleRegex := regexp.MustCompile(`\D+`)
+	result := sampleRegex.ReplaceAllString(value, "")
+	return result
 }
--- cpf/cpf_test.go
+++ cpf/cpf_test.go
@@ -7,12 +7,21 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-func TestNewCPF(t *testing.T) {
-	expectedCPF := "896.130.470-42"
+func TestNewCPFWithoutSpecialCharacters(t *testing.T) {
+	expectedCPF := "89613047042"
 
-	cpf, error := cpf.NewCPF("896.130.470-42")
+	cpf, error := cpf.NewCPF("89613047042")
 
 	assert.Equal(t, nil, error)
+	assert.Equal(t, cpf.Value, expectedCPF)
+}
+
+func TestNewCPFWithSpecialCharacters(t *testing.T) {
+	expectedCPF := "896.130.470-42"
+
+	cpf, err := cpf.NewCPF("896.130.470-42")
+
+	assert.Equal(t, nil, err)
 	assert.Equal(t, cpf.Value, expectedCPF)
 }
 
@@ -22,4 +31,12 @@ func TestCPFCannotBeNull(t *testing.T) {
 	_, err := cpf.NewCPF("")
 
 	assert.EqualError(t, err, expectError)
+}
+
+func TestCPFHasInvalidLength(t *testing.T) {
+	expectedError := "invalid CPF size"
+
+	_, err := cpf.NewCPF("896.3858")
+
+	assert.EqualError(t, err, expectedError)
 }
